7eed5c8397f0f5f6272db94ae18b2dce22ca578a
5ouma <101255979+5ouma@users.noreply.github.com>
2022-04-23 19:25:30 +0900

    [add] Separate javascript file
    Create new javascript file from inline code in index.html.

---
+++ getAppearance.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches === false) {
+        document.getElementById("appearanceHed").textContent = "Light Mode";
+    } else if (
+        window.matchMedia("(prefers-color-scheme: dark)").matches === true
+    ) {
+        document.getElementById("appearanceHed").textContent = "Dark Mode";
+    } else {
+        document.getElementById("appearanceHed").textContent =
+            "It doesn't get Appearance Mode";
+    }
+}, 300);

a0839dbda012e71436aadddd295be7cd26aacba5
Souma <101255979+5ouma@users.noreply.github.com>
2025-08-30 20:29:51 +0900

    refactor(public): Bind the update function to change event
    Only when appearance mode is changed, update the text.

--- public/script.js
+++ public/script.js
@@ -1,12 +1,13 @@
-setInterval(() => {
-  const text = document.getElementsByTagName("h1")[0];
-  if (globalThis.matchMedia("(prefers-color-scheme: light)").matches === true) {
-    text.textContent = "Light Mode";
-  } else if (
-    globalThis.matchMedia("(prefers-color-scheme: dark)").matches === true
-  ) {
-    text.textContent = "Dark Mode";
-  } else {
-    text.textContent = "No Pref";
-  }
-}, 300);
+const element = document.querySelector("h1");
+const lightMode = globalThis.matchMedia("(prefers-color-scheme: light)");
+const darkMode = globalThis.matchMedia("(prefers-color-scheme: dark)");
+const updateText = (element, lightMode, darkMode) => {
+  if (lightMode.matches === true) element.textContent = "Light Mode";
+  else if (darkMode.matches === true) element.textContent = "Dark Mode";
+  else element.textContent = "No Pref";
+};
+
+lightMode.addEventListener("change", () => {
+  updateText(element, lightMode, darkMode);
+});
+updateText(element, lightMode, darkMode);
